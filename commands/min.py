33de695f20d1382bdf8bf5578d9515f1e82e849f
Julia <250353@edu.p.lodz.pl>
2025-10-21 16:29:39 +0200

    min max z parametrami

--- commands/min.py
+++ commands/min.py
@@ -1,19 +1,23 @@
 import numpy as np
-from commands.adaptive import _create_window
 
 def do_min_filer(img: np.ndarray, args: dict) -> np.ndarray:
+    window_size_str = args.get("-window", 3)
+    window_size = int(window_size_str)
+    if (window_size%2==0 or window_size%2==0):
+        raise ValueError("Window size cannot be a number divisible by 2")
+    m = int((window_size - 1) / 2)
     img2 = np.copy(img)
     if img.ndim == 3:
         for c in range(img.shape[2]):
-            for i in range(1, img.shape[0] - 1):
-                for j in range(1, img.shape[1] - 1):
-                    window = img[i-1:i+2, j-1:j+2, c]
+            for i in range(m, img.shape[0] - m):
+                for j in range(m, img.shape[1] - m):
+                    window = img[i-m:i+m+1, j-m:j+m+1, c]
                     min_val = np.min(window)
                     img2[i, j, c] = min_val
     else:
-        for i in range(1, img.shape[0] - 1):
-            for j in range(1, img.shape[1] - 1):
-                window = img[i-1:i+2, j-1:j+2]
+        for i in range(m, img.shape[0] - m):
+            for j in range(m, img.shape[1] - m):
+                window = img[i-m:i+m+1, j-m:j+m+1]
                 min_val = np.min(window)
                 img2[i, j] = min_val
     return img2
